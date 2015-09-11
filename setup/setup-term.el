97108bdb833097b84d6a36d5e4f0c489b8210224
AbyChan <crystal.tyan@gmail.com>
2015-09-11 20:00:33 +0800

    update

--- setup/setup-term.el
+++ setup/setup-term.el
@@ -7,10 +7,6 @@
           (lambda ()
             (setq term-buffer-maximum-size 10000)))
 
-(add-hook 'term-mode-hook
-          (lambda ()
-            (setq show-trailing-whitespace nil)
-            (autopair-mode -1)))
 
 (defcustom term-unbind-key-list
   '("C-z" "C-x" "C-c" "C-h" "C-y" "<ESC>")
