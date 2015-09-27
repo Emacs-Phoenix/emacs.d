bc2eddcb6bf85d1cf14ae1caab478d7fb7623047
AbyChan <crystal.tyan@gmail.com>
2015-09-28 00:03:12 +0800

    writting emacs-boilerplate

--- undohist/!home!tyan!.emacs.d!setup!setup-spell.el
+++
@@ -1,2 +0,0 @@
-
-((digest . "78f537defa117c596bfa0cb522dd2cac") (undo-list (4878 . 4879) 4871 nil (4866 . 4871) nil ("se" . -4866) nil (nil rear-nonsticky nil 4874 . 4875) (nil fontified nil 1 . 4875) (1 . 4875) (t . -1)))
