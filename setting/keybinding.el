f24686df84b66e60cfa3761b0c32f9b224a6c685
AbyChan <crystal.tyan@gmail.com>
2015-12-16 09:55:48 +0800

    keybinding for mac keyborad

--- setting/keybinding.el
+++ setting/keybinding.el
@@ -197,4 +197,14 @@
 ;;  For ErgoDox
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  For Mac keyboard
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(global-set-key (kbd "H-'") 'previous-buffer)
+(global-set-key (kbd "H-;") 'beginning-of-buffer)
+(global-set-key (kbd "H-/") 'next-buffer)
+(global-set-key (kbd "H-.") 'end-of-buffer)
+
+
+
 (provide 'keybinding)
