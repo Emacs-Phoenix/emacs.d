5f60be693fd5aca89bc6d1789b40e780e993b1f7
AbyChan <crystal.tyan@gmail.com>
2015-12-16 19:04:50 +0800

    update

--- setting/default.el
+++ setting/default.el
@@ -167,5 +167,14 @@
 
 (auto-save-mode -1)
 
+;; Save all tempfiles in $TMPDIR/emacs$UID/                                                        
+(defconst emacs-tmp-dir (format "%s/%s%s/" temporary-file-directory "emacs" (user-uid)))
+(setq backup-directory-alist
+      `((".*" . ,emacs-tmp-dir)))
+(setq auto-save-file-name-transforms
+      `((".*" ,emacs-tmp-dir t)))
+(setq auto-save-list-file-prefix
+      emacs-tmp-dir)
+
 (provide 'default)
 
