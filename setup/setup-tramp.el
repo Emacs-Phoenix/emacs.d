687255f77d871910d5613adcdaff7f277cbffa1b
AbyChan <crystal.tyan@gmail.com>
2016-04-15 00:08:25 +0800

    imenu-list

--- setup/setup-tramp.el
+++ setup/setup-tramp.el
@@ -1,13 +1,4 @@
 (require 'tramp)
 (require 'tramp-cache)
 
-(setq tramp-default-method "ssh")
-(setq tramp-debug-buffer t)
-(setq tramp-verbose 9)
-(setq tramp-default-method "ssh")
-(setq tramp-password-prompt-regexp ".*[Pp]assword: *$")
-(setq tramp-shell-prompt-pattern "^[^;$#>]*[;$#>] *")
-(setq password-cache-expiry nil)
-
-
 (provide 'setup-tramp)
