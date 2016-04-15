5e586db629f470852b14405c80b650fb85222f8f
AbyChan <crystal.tyan@gmail.com>
2016-04-15 10:23:20 +0800

    configure

--- setup/setup-org.el
+++ setup/setup-org.el
@@ -3,6 +3,9 @@
 ;; (setq org-todo-keywords
 ;;       '((sequence "TODO(t)" "WAIT(w@/!)" "|" "DONE(d!)" "CANCELED(c@)")))
 
+
+(add-hook 'org-mode-hook (lambda () (setq truncate-lines t)))
+
 (setq org-todo-keywords '((type "TODO" "PREPARE" "WAIT" "DOING" "DONE" "TESTING" "FIXING" "CANCELED")))
 
 (setq org-todo-keyword-faces
