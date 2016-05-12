6b6765f1ff1cc208880ad01d44aee646db267838
AbyChan <crystal.tyan@gmail.com>
2016-05-13 00:40:27 +0800

    ejs mode mapping

--- setting/mode-mapping.el
+++ setting/mode-mapping.el
@@ -14,6 +14,7 @@
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
