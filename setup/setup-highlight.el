c29515c72da005eca675a927d71921bce6ca979a
AbyChan <crystal.tyan@gmail.com>
2016-05-12 18:09:14 +0800

    add advice after use highlight-symbol-at-point

--- setup/setup-highlight.el
+++ setup/setup-highlight.el
@@ -5,4 +5,8 @@
 ;;(add-hook 'css-mode-hook 'auto-highlight-symbol-mode)
 (add-hook 'emacs-lisp-mode-hook 'auto-highlight-symbol-mode)
 
+(advice-add 'highlight-symbol-at-point :before
+            (lambda ()
+              (message "You can unhighlight by use \"unhighlight-regexp\"")))
+
 (provide 'setup-highlight)
