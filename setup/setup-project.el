5bef4b8d6e2484c6e0b7cf7dc2b7ad00fb995c22
AbyChan <crystal.tyan@gmail.com>
2016-06-30 23:10:32 +0800

    initial projectile

--- setup/setup-project.el
+++ setup/setup-project.el
@@ -4,4 +4,13 @@
 (global-unset-key (kbd "M-p"))
 (global-set-key (kbd "M-p") 'projectile-find-file)
 
+(defun projectile-find-file-or-from-region ()
+  (interactive)
+  (if (region-active-p)
+      ;;TODO
+      (message "h")
+    (projectile-find-file)))
+
+(global-set-key (kbd "H-n") 'projectile-find-file-or-from-region)
+
 (provide 'setup-project)
