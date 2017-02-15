39b2c8c3d223e15c22c90846a76426a6704646cb
fwchen <crystal.tyan@gmail.com>
2017-02-15 15:47:05 +0800

    refactor package to use package manager

--- init.el
+++ init.el
@@ -1,3 +1,26 @@
+;; TODO module it
+(require 'package)
+(setq package-list '(js2-mode js2-refactor web-mode hungry-delete helm-swoop
+                              haskell-mode elfeed imenu-anywhere emmet-mode
+                              guide-key dumb-jump clojure-mode powerline
+                              ido-ubiquitous magit coffee-mode))
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+
+;; fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;; -----------------------------------------
+
 (setq initial-frame-alist
       '(
         (width . 170) ; character
