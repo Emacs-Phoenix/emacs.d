108d7b8daf7dfb46cd06bbdf5fa4c48107d45601
fwchen <crystal.tyan@gmail.com>
2017-02-26 12:57:16 +0800

    update interactive function name

--- setup/setup-feed.el
+++ setup/setup-feed.el
@@ -1,16 +1,14 @@
 (require 'emacs-feedjs)
 
-
 (setq feedjs--process "/Users/chchen/MY-PROJECT/NodeAtom/app.js")
 
 (define-prefix-command 'emacs-feed-key-map)
 (global-set-key (kbd "C-c e") 'emacs-feed-key-map)
 (define-key emacs-feed-key-map (kbd "s") 'feedjs)
 (define-key emacs-feed-key-map (kbd "k") 'kill-feedjs-process)
-(define-key emacs-feed-key-map (kbd "r") 'restart-feedjs-process)
-(define-key emacs-feed-key-map (kbd "n") 'start-feedjs-notify)
+(define-key emacs-feed-key-map (kbd "r") 'feedjs-restart-process)
+(define-key emacs-feed-key-map (kbd "n") 'feedjs-stop-notify)
 
 ;;(run-feedjs)
-
 
 (provide 'setup-feed)
