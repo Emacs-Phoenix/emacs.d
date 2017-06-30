ba25fd5da981476fad33ae527dbd5acad4b8684e
fwchen <crystal.tyan@gmail.com>
2017-06-30 16:02:45 +0800

    prettier

--- setup/setup-prettier.el
+++ setup/setup-prettier.el
@@ -1,16 +1,21 @@
 (require 'prettier-js)
-(add-hook 'js2-mode-hook 'prettier-js-mode)
-(add-hook 'web-mode-hook 'prettier-js-mode)
 
-(defun enable-minor-mode (my-pair)
-  "Enable minor mode if filename match the regexp.  MY-PAIR is a cons cell (regexp . minor-mode)."
-  (if (buffer-file-name)
-      (if (string-match (car my-pair) buffer-file-name)
-          (funcall (cdr my-pair)))))
+(setq prettier-js-args '(
+                         "--trailing-comma" "all"
+                         "--single-quote"
+                         ))
+;; (add-hook 'js2-mode-hook 'prettier-js-mode)
+;; (add-hook 'web-mode-hook 'prettier-js-mode)
 
-(add-hook 'web-mode-hook #'(lambda ()
-                             (enable-minor-mode
-                              '("\\.jsx?\\'" . prettier-js-mode))))
+;; (defun enable-minor-mode (my-pair)
+;;   "Enable minor mode if filename match the regexp.  MY-PAIR is a cons cell (regexp . minor-mode)."
+;;   (if (buffer-file-name)
+;;       (if (string-match (car my-pair) buffer-file-name)
+;;           (funcall (cdr my-pair)))))
+
+;; (add-hook 'web-mode-hook #'(lambda ()
+;;                              (enable-minor-mode
+;;                               '("\\.jsx?\\'" . prettier-js-mode))))
 
 
 (provide 'setup-prettier)
