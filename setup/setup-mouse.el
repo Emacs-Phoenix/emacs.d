d840712272e430ea8bf3f2bcffe6d2f311e70550
fwchen <crystal.tyan@gmail.com>
2017-08-18 09:40:36 +0800

    disable mouse event(needless)

---
+++ setup/setup-mouse.el
@@ -0,0 +1,16 @@
+(define-minor-mode disable-mouse-mode
+  "A minor-mode that disables all mouse keybinds."
+  :global t
+  :lighter " 🐭"
+  :keymap (make-sparse-keymap))
+
+(dolist (type '(mouse down-mouse drag-mouse
+                      double-mouse triple-mouse))
+  (dolist (prefix '("" C- M- S- M-S- C-M- C-S- C-M-S-))
+    ;; Yes, I actually HAD to go up to 7 here.
+    (dotimes (n 7)
+      (let ((k (format "%s%s-%s" prefix type n)))
+        (define-key disable-mouse-mode-map
+          (vector (intern k)) #'ignore)))))
+
+(provide 'setup-mouse)
